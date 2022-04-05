83abe68caf1a60cfc661bf0fe250cbfe9631f5de
Jimmy Wu <wuzhijing0127@gmail.com>
2022-04-05 19:47:16 -0400

    sum game

--- Number.py
+++ Number.py
@@ -1,1 +1,5 @@
-print ('abc')
+print ('hello math genius, let us play the game')
+a = int(input('type the first number: '))
+b = int(input('type the second number: '))
+sum = a + b
+print("the sum of the two numbers is: " + str(sum))
